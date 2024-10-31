95c34b996bb0a566e19d877fd5bc3ebd343af6d1
leroy <gmail@leroytop.com>
2024-10-31 10:47:07 +0800

    fix: eslintignore

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const ignorePath = path.resolve(__dirname, "eslintignore");
+const ignorePath = path.resolve(__dirname, ".prettierignore");
 
 export default tseslint.config(
   js.configs.recommended,
