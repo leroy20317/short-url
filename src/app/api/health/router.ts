fb18168f505880f145c4b416439e85c7761afba8
leroy <gmail@leroytop.com>
2025-04-18 11:02:40 +0800

    chore: 优化 deploy.yml 中的备份和健康检查流程

---
+++ src/app/api/health/router.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: leroy
+ * @Date: 2025-04-18 10:59:27
+ * @LastEditTime: 2025-04-18 11:00:00
+ * @Description: 健康检查
+ */
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  return NextResponse.json({ status: 'ok' });
+}
